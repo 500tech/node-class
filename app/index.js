699afe1fd429883cafde56828516db4d990d6113
oakfang <oakfang@gmail.com>
2019-07-01 10:16:08 +0300

    Rendering views

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+app.set("view engine", "pug");
 app.use(morgan("combined"));
 app.use(express.json());
 
@@ -14,6 +15,10 @@ app.get("/", (_req, res) => {
   return res.json({
     message: "Hello, world"
   });
+});
+
+app.get("/foo", (_req, res) => {
+  return res.render("index");
 });
 
 module.exports = app;
